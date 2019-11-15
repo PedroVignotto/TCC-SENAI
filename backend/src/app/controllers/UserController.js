d24bdce9ecfee45f90f6b339834337ab3442c8a6
PedroVignotto <pedro.vignotto@hotmail.com>
2019-11-14 21:15:42 -0300

    Cadastrando e logando usuários

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -24,9 +24,9 @@ class UserController {
       return res.status(400).json({ error: 'User already exists' });
     }
 
-    const { id, name, email, provider } = await User.create(req.body);
+    const { id, name, email } = await User.create(req.body);
 
-    return res.json({ id, name, email, provider });
+    return res.json({ id, name, email });
   }
 
   async update(req, res) {
