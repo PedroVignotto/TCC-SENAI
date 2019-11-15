123d2b6ebdb08bd16e97f53cbfdd6138b9aa0a17
PedroVignotto <pedro.vignotto@hotmail.com>
2019-11-15 13:25:51 -0300

    Iniciando frontend

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
+
+export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmar senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
+}
---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No minimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Seu nome completo" />
+        <Input name="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
